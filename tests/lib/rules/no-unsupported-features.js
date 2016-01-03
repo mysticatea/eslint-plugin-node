04b9a4c76b70744a7f1bb3fefb508d56befe8541
Toru Nagashima <star.ctor@gmail.com>
2016-01-03 16:56:46 +0900

    Fix: some trivial things.

--- tests/lib/rules/no-unsupported-features.js
+++ tests/lib/rules/no-unsupported-features.js
@@ -1,5 +1,4 @@
 /**
- * @fileoverview Tests for restrict-syntax rule.
  * @author Toru Nagashima
  * @copyright 2015 Toru Nagashima. All rights reserved.
  * See LICENSE file in root directory for full license.
