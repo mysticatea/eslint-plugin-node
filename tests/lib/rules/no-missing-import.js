390c9ec4c8c89b9f65699d6e00a7a11fabd591be
Toru Nagashima <star.ctor@gmail.com>
2017-01-31 20:45:27 +0900

    Chore: simpify tests for es modules

--- tests/lib/rules/no-missing-import.js
+++ tests/lib/rules/no-missing-import.js
@@ -30,69 +30,56 @@ function fixture(name) {
 // Test
 //------------------------------------------------------------------------------
 
-
-const ruleTester = new RuleTester()
+const ruleTester = new RuleTester({parserOptions: {sourceType: "module"}})
 ruleTester.run("no-missing-import", rule, {
     valid: [
         {
             filename: fixture("test.js"),
             code: "import eslint from 'eslint';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import fs from 'fs';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import eslint from 'eslint/lib/ast-utils';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import a from './a'; a();",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import a from './a.js';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import aConfig from './a.config';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import aConfig from './a.config.js';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import b from './b';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import b from './b.json';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import c from './c.coffee';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import mocha from 'mocha';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import mocha from 'mocha!foo?a=b&c=d';",
-            parserOptions: {sourceType: "module"},
         },
 
         // tryExtensions
@@ -100,64 +87,49 @@ ruleTester.run("no-missing-import", rule, {
             filename: fixture("test.js"),
             code: "import c from './c';",
             options: [{tryExtensions: [".coffee"]}],
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import c from './c';",
             settings: {node: {tryExtensions: [".coffee"]}},
-            parserOptions: {sourceType: "module"},
         },
 
         // Ignores it if the filename is unknown.
-        {
-            code: "import abc from 'no-exist-package-0';",
-            parserOptions: {sourceType: "module"},
-        },
-        {
-            code: "import b from './b';",
-            parserOptions: {sourceType: "module"},
-        },
+        "import abc from 'no-exist-package-0';",
+        "import b from './b';",
 
         // no source.
         {
             filename: fixture("test.js"),
             code: "export {foo, bar};",
-            parserOptions: {sourceType: "module"},
         },
 
         // Should work fine if the filename is relative.
         {
             filename: "tests/fixtures/no-missing/test.js",
             code: "import eslint from 'eslint'",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: "tests/fixtures/no-missing/test.js",
             code: "import a from './a';",
-            parserOptions: {sourceType: "module"},
         },
 
         // Relative paths to a directory should work.
         {
             filename: fixture("test.js"),
             code: "import a from '.';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import a from './';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import a from './foo';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("test.js"),
             code: "import a from './foo/';",
-            parserOptions: {sourceType: "module"},
         },
 
         // allow option.
@@ -165,38 +137,32 @@ ruleTester.run("no-missing-import", rule, {
             filename: fixture("test.js"),
             code: "import electron from 'electron';",
             options: [{allowModules: ["electron"]}],
-            parserOptions: {sourceType: "module"},
         },
     ],
     invalid: [
         {
             filename: fixture("test.js"),
             code: "import abc from 'no-exist-package-0';",
-            parserOptions: {sourceType: "module"},
             errors: ["\"no-exist-package-0\" is not found."],
         },
         {
             filename: fixture("test.js"),
             code: "import test from '@mysticatea/test';",
-            parserOptions: {sourceType: "module"},
             errors: ["\"@mysticatea/test\" is not found."],
         },
         {
             filename: fixture("test.js"),
             code: "import c from './c';",
-            parserOptions: {sourceType: "module"},
             errors: ["\"./c\" is not found."],
         },
         {
             filename: fixture("test.js"),
             code: "import d from './d';",
-            parserOptions: {sourceType: "module"},
             errors: ["\"./d\" is not found."],
         },
         {
             filename: fixture("test.js"),
             code: "import a from './a.json';",
-            parserOptions: {sourceType: "module"},
             errors: ["\"./a.json\" is not found."],
         },
 
@@ -204,13 +170,11 @@ ruleTester.run("no-missing-import", rule, {
         {
             filename: "tests/fixtures/no-missing/test.js",
             code: "import eslint from 'no-exist-package-0';",
-            parserOptions: {sourceType: "module"},
             errors: ["\"no-exist-package-0\" is not found."],
         },
         {
             filename: "tests/fixtures/no-missing/test.js",
             code: "import c from './c';",
-            parserOptions: {sourceType: "module"},
             errors: ["\"./c\" is not found."],
         },
 
@@ -218,13 +182,11 @@ ruleTester.run("no-missing-import", rule, {
         {
             filename: fixture("test.js"),
             code: "import a from './bar';",
-            parserOptions: {sourceType: "module"},
             errors: ["\"./bar\" is not found."],
         },
         {
             filename: fixture("test.js"),
             code: "import a from './bar/';",
-            parserOptions: {sourceType: "module"},
             errors: ["\"./bar/\" is not found."],
         },
     ],
--- tests/lib/rules/no-unpublished-import.js
+++ tests/lib/rules/no-unpublished-import.js
@@ -30,100 +30,82 @@ function fixture(name) {
 // Test
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester()
+const ruleTester = new RuleTester({parserOptions: {sourceType: "module"}})
 ruleTester.run("no-unpublished-import", rule, {
     valid: [
         {
             filename: fixture("1/test.js"),
             code: "import fs from 'fs';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("1/test.js"),
             code: "import aaa from 'aaa'; aaa();",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("1/test.js"),
             code: "import c from 'aaa/a/b/c';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("1/test.js"),
             code: "import a from './a';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("1/test.js"),
             code: "import a from './a.js';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("2/ignore1.js"),
             code: "import test from './test';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("2/ignore1.js"),
             code: "import bbb from 'bbb';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("2/ignore1.js"),
             code: "import c from 'bbb/a/b/c';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("2/ignore1.js"),
             code: "import ignore2 from './ignore2';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/test.js"),
             code: "import a from './pub/a';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/test.js"),
             code: "import test2 from './test2';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/test.js"),
             code: "import aaa from 'aaa';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/test.js"),
             code: "import bbb from 'bbb';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/pub/ignore1.js"),
             code: "import bbb from 'bbb';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/pub/test.js"),
             code: "import p from '../package.json';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/src/pub/test.js"),
             code: "import bbb from 'bbb';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/src/pub/test.js"),
             code: "import bbb from 'bbb!foo?a=b&c=d';",
-            parserOptions: {sourceType: "module"},
         },
 
         // `convertPath` option.
         {
             filename: fixture("3/src/test.jsx"),
             code: "import a from './a';",
-            parserOptions: {sourceType: "module"},
             settings: {
                 node: {
                     convertPath: {"src/**/*.jsx": ["src/(.+?)\\.jsx", "pub/$1.js"]},
@@ -134,7 +116,6 @@ ruleTester.run("no-unpublished-import", rule, {
         {
             filename: fixture("3/src/test.jsx"),
             code: "import a from './a';",
-            parserOptions: {sourceType: "module"},
             options: [{
                 convertPath: {"src/**/*.jsx": ["src/(.+?)\\.jsx", "pub/$1.js"]},
                 tryExtensions: [".js", ".jsx", ".json"],
@@ -142,32 +123,23 @@ ruleTester.run("no-unpublished-import", rule, {
         },
 
         // Ignores it if the filename is unknown.
-        {
-            code: "import noExistPackage0 from 'no-exist-package-0';",
-            parserOptions: {sourceType: "module"},
-        },
-        {
-            code: "import b from './b';",
-            parserOptions: {sourceType: "module"},
-        },
+        "import noExistPackage0 from 'no-exist-package-0';",
+        "import b from './b';",
 
         // Should work fine if the filename is relative.
         {
             filename: "tests/fixtures/no-unpublished/2/test.js",
             code: "import aaa from 'aaa';",
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: "tests/fixtures/no-unpublished/2/test.js",
             code: "import a from './a';",
-            parserOptions: {sourceType: "module"},
         },
 
         {
             filename: fixture("1/test.js"),
             code: "import electron from 'electron';",
             options: [{allowModules: ["electron"]}],
-            parserOptions: {sourceType: "module"},
         },
     ],
     invalid: [
@@ -175,66 +147,55 @@ ruleTester.run("no-unpublished-import", rule, {
             filename: fixture("1/test.js"),
             code: "import noDeps from 'no-deps';",
             errors: ["\"no-deps\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("1/test.js"),
             code: "import c from 'no-deps/a/b/c';",
             errors: ["\"no-deps\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("2/test.js"),
             code: "import ignore1 from './ignore1.js';",
             errors: ["\"./ignore1.js\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("2/test.js"),
             code: "import ignore1 from './ignore1';",
             errors: ["\"./ignore1\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("2/ignore1.js"),
             code: "import noDeps from 'no-deps';",
             errors: ["\"no-deps\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/test.js"),
             code: "import noDeps from 'no-deps';",
             errors: ["\"no-deps\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/pub/test.js"),
             code: "import bbb from 'bbb';",
             errors: ["\"bbb\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/pub/test.js"),
             code: "import ignore1 from './ignore1';",
             errors: ["\"./ignore1\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/pub/test.js"),
             code: "import abc from './abc';",
             errors: ["\"./abc\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/pub/test.js"),
             code: "import test from '../test';",
             errors: ["\"../test\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: fixture("3/pub/test.js"),
             code: "import a from '../src/pub/a.js';",
-            parserOptions: {sourceType: "module"},
             errors: ["\"../src/pub/a.js\" is not published."],
         },
 
@@ -242,7 +203,6 @@ ruleTester.run("no-unpublished-import", rule, {
             filename: fixture("1/test.js"),
             code: "import a from '../a.js';",
             errors: ["\"../a.js\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
 
         // Should work fine if the filename is relative.
@@ -250,13 +210,11 @@ ruleTester.run("no-unpublished-import", rule, {
             filename: "tests/fixtures/no-unpublished/2/test.js",
             code: "import noDeps from 'no-deps';",
             errors: ["\"no-deps\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
         {
             filename: "tests/fixtures/no-unpublished/2/test.js",
             code: "import ignore1 from './ignore1';",
             errors: ["\"./ignore1\" is not published."],
-            parserOptions: {sourceType: "module"},
         },
     ],
 })
