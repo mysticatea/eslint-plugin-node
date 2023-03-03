e43d4ea0b85e7415ea073690f37e9eb51c78b090
唯然 <weiran.zsd@outlook.com>
2023-03-03 11:38:31 +0800

    Revert "fix: Updated `import-meta-resolve` & pack it automatically (#87)" (#88)
    This reverts commit f74d35e1c62a45773eb5693379a5725842179a39.

--- scripts/convert-pure-esm-to-cjs.js
+++ scripts/convert-pure-esm-to-cjs.js
@@ -1,6 +1,6 @@
 const { execSync } = require("child_process")
 
 execSync(
-    "esbuild --platform=node --external:builtins --bundle node_modules/import-meta-resolve/lib/resolve.js > converted-esm/import-meta-resolve.js",
+    "esbuild --platform=node --external:builtins --bundle node_modules/import-meta-resolve/lib/resolve.js > lib/converted-esm/import-meta-resolve.js",
     { shell: true }
 )
