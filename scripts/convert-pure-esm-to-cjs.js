6573d7a08bd57229726241235722da32994350f8
唯然 <weiran.zsd@outlook.com>
2023-04-18 15:49:50 +0800

    fix: upgrade import-meta-resolve v2

--- scripts/convert-pure-esm-to-cjs.js
+++ scripts/convert-pure-esm-to-cjs.js
@@ -1,6 +1,6 @@
 const { execSync } = require("child_process")
 
 execSync(
-    "esbuild --platform=node --external:builtins --bundle node_modules/import-meta-resolve/lib/resolve.js > lib/converted-esm/import-meta-resolve.js",
+    "./node_modules/.bin/esbuild --platform=node --external:builtins --bundle node_modules/import-meta-resolve/lib/resolve.js > lib/converted-esm/import-meta-resolve.js",
     { shell: true }
 )
